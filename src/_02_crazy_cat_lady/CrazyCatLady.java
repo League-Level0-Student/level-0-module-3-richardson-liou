8dc5637951d924fd3457278e9c7dd057bfc7b41f
League Student <league@iMac-34.attlocal.net>
2020-03-04 20:00:10 -0800

    ..

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,14 +5,24 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cats = JOptionPane.showInputDialog("How many cats do you have");
 		// 2. Convert their answer into an int
-
+int  number = Integer.parseInt(cats);
+	if (number <= 0-3 ) {
+		playVideo("https://www.youtube.com/watch?v=p26uKxEAoaQ");
+}
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+	else if (number >=3) {
+		JOptionPane.showMessageDialog(null, "YOU ARE A CRAZY CAT LADY");
+	}
+	else if (number == 0){
+		playVideo("https://www.youtube.com/watch?v=bKCPEdRimjo");
+	}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
---
+++ src/unbirthday.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+
+
+		public static void main(String[] args) {
+String food1 = "sauce";
+String food2 = "sauce";
+String babyfood = "applesauce";
+String food3 = babyfood.substring(5);
+
+	if (food3 == "sauce") {
+		System.out.println("F");
+	}
+
+		}
+
+	}
+
